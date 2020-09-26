3a8c541e9f572bdadb98313e6fe9061e698772ef
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2020-09-26 18:42:00 -0400

    feat
    users can created vistis

--- src/services/Visit.service.js
+++ src/services/Visit.service.js
@@ -1,34 +1,34 @@
 import Visit from '../models/Visit';
-
+import User from '../models/User';
+import userService from './User.service';
 class VisitService {
 
   static async create(params) {
     let { newEntity } = params;
+
+    const visitor = await User.findOne({ where: { code: newEntity.visitor.code } });
+    if (!visitor) {
+      newEntity.visitor = await userService.create({ newEntity: newEntity.visitor });
+    } else[
+      newEntity.visitor.id = visitor.id
+    ]
+
+    const receiver = await User.findOne({ where: { code: newEntity.receiver.code } });
+    if (!receiver) {
+      newEntity.receiver = await userService.create({ newEntity: newEntity.receiver });
+    } else[
+      newEntity.receiver.id = receiver.id
+    ]
+
     try {
-      const entityCreated = await Visit.create(newEntity);
+      const visit = {
+        reasonVisit: newEntity.reasonVisit,
+        userVisitorId: newEntity.visitor.id,
+        userReceiverId: newEntity.receiver.id
+      }
+
+      const entityCreated = await Visit.create(visit);
       return entityCreated;
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  static async getAll(params) {
-    const { criterions } = params;
-
-    try {
-      const { rows } = await Visit.findAndCountAll({ ...criterions });
-      return { rows, count: rows.length };
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  static async getById(params) {
-    const { id } = params;
-
-    try {
-      const entity = await Visit.findOne({ where: { id } })
-      return entity;
     } catch (error) {
       throw error;
     }
